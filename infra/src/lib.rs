955d493451a8840a97a958c88e4d01522f56c8d5
kulapoo <kulaskulapoo@gmail.com>
2025-08-04 11:43:31 +0800

    add crabnebula

--- infra/src/lib.rs
+++ infra/src/lib.rs
@@ -13,9 +13,18 @@ use usecases::handle_work_session_completed;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
+    #[cfg(debug_assertions)] // only enable instrumentation in development builds
+    let devtools = tauri_plugin_devtools::init();
     let task_repository: TaskRepositoryArc = Arc::new(InMemoryTaskRepository::with_default_task());
 
-    tauri::Builder::default()
+    let mut builder = tauri::Builder::default();
+
+    #[cfg(debug_assertions)]
+    {
+        builder = builder.plugin(devtools);
+    }
+
+    builder
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_fs::init())
