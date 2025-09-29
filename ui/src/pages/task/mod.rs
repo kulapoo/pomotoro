273dd5a2c73712847edd692be9b4daddc93e88e8
kulapoo <kulaskulapoo@gmail.com>
2025-09-29 13:44:46 +0800

    update

--- ui/src/pages/task/mod.rs
+++ ui/src/pages/task/mod.rs
@@ -3,10 +3,12 @@ mod task_list;
 mod task_page;
 mod task_search;
 mod task_state;
+mod task_update_form;
 pub mod task_vm;
 
 pub use task_creation_form::TaskCreationForm;
 pub use task_list::TaskList;
 pub use task_page::TaskPage;
 pub use task_search::TaskSearch;
+pub use task_update_form::TaskUpdateForm;
 pub use task_vm::{TasksViewModel, TaskDto};
